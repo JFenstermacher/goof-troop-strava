7e81498a34ba839e91822d996eeeb69f48aa7d4b
Jonathan Fenstermacher <jfenstermacher123@gmail.com>
2022-05-24 18:00:50 -0400

    basic instance stood up

---
+++ terraform/main.tf
@@ -0,0 +1,112 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.56.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~>3.1.0"
+    }
+  }
+}
+
+provider "tls" {}
+provider "aws" {
+  region = var.region
+}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+}
+
+resource "tls_private_key" "ssh" {
+  algorithm = "RSA"
+  rsa_bits  = "4096"
+}
+
+resource "local_file" "private_key" {
+  content         = tls_private_key.ssh.private_key_pem
+  filename        = "${aws_key_pair.this.key_name}.pem"
+  file_permission = "0600"
+}
+
+resource "aws_key_pair" "this" {
+  key_name_prefix = var.prefix
+  public_key      = tls_private_key.ssh.public_key_openssh
+}
+
+resource "aws_security_group" "this" {
+  name_prefix = var.prefix
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_iam_role" "this" {
+  name_prefix = "${var.prefix}-role"
+  path        = "/"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Effect = "Allow"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_instance_profile" "this" {
+  name_prefix = "${var.prefix}-ssm-instance-profile"
+  role        = aws_iam_role.this.name
+}
+
+resource "aws_instance" "this" {
+
+  ami                         = data.aws_ami.amazon_linux.id
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  key_name                    = aws_key_pair.this.key_name
+  iam_instance_profile        = aws_iam_instance_profile.this.name
+  vpc_security_group_ids      = [aws_security_group.this.id]
+
+  tags = {
+    Name = var.prefix
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  type = string
+
+  default = "us-east-1"
+}
+
+variable "prefix" {
+  type = string
+
+  default = "goof-troop"
+}
